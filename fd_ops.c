4f43eef3bfd4504feda693cfacec0ed5ee7f24f6
nobodyxu <Jiahao_XU@outlook.com>
2020-08-03 18:17:01 +1000

    Add builtin fd_ops.c

---
+++ fd_ops.c
@@ -0,0 +1,65 @@
+/* fd_ops - loadable builtin that defines fd-related functions */
+
+/* See Makefile for compilation details. */
+#include "bash/config.h"
+
+#include <unistd.h>
+#include "bashansi.h"
+#include <stdio.h>
+#include <errno.h>
+
+#include "loadables.h"
+
+extern char *strerror ();
+
+int fd_ops_builtin (list)
+    WORD_LIST *list;
+{
+    int rval;
+
+    rval = EXECUTION_SUCCESS;
+    reset_internal_getopt ();
+
+    for (int opt; (opt = internal_getopt (list, "")) != -1; ) {
+        switch (opt) {
+            CASE_HELPOPT;
+
+        default:
+            builtin_usage();
+            return (EX_USAGE);
+        }
+    }
+    list = loptend;
+
+    return (rval);
+}
+
+/* Called when `template' is enabled and loaded from the shared object.  If this
+   function returns 0, the load fails. */
+PUBLIC int fd_ops_builtin_load (name)
+    char *name;
+{
+    return (1);
+}
+
+/* Called when `template' is disabled. */
+PUBLIC void fd_ops_builtin_unload (name)
+     char *name;
+{
+}
+
+char *fd_ops_doc[] = {
+    "Short description.",
+    ""
+    "Longer description of builtin and usage.",
+    (char *)NULL
+};
+
+PUBLIC struct builtin fd_ops_struct = {
+    "fd_ops",         /* builtin name */
+    fd_ops_builtin,       /* function implementing the builtin */
+    BUILTIN_ENABLED,        /* initial flags for builtin */
+    fd_ops_doc,           /* array of long documentation strings. */
+    "fd_ops",         /* usage synopsis; becomes short_doc */
+    0               /* reserved for internal use */
+};
