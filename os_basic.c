41b465b9c1e1c1619ceda499e435222ea5393b5c
nobodyxu <Jiahao_XU@outlook.com>
2020-08-11 19:37:09 +1000

    Add impl detail in doc for recvfds_builtin

--- os_basic.c
+++ os_basic.c
@@ -1248,6 +1248,10 @@ PUBLIC struct builtin recvfds_struct = {
         "On error:",
         "    If no cmsg is received, returns 4;",
         "    If the cmsg received isn't the type that contains fds, returns 3.",
+        "",
+        "Implemention detail:",
+        "    recvfds would consume one byte from the unix socket and the fds associated with this byte, ",
+        "    due to the reason described in sendfds' documentation.",
         (char*) NULL
     },                            /* array of long documentation strings. */
     "recvfds [-C] <int> fd_of_unix_socket nfd var",      /* usage synopsis; becomes short_doc */
