7b1d8fef19b11ae987480d389f815179548422be
nobodyxu <Jiahao_XU@outlook.com>
2020-08-12 23:01:50 +1000

    Rm commented function prototype in bind_builtin

--- os_basic.c
+++ os_basic.c
@@ -1821,17 +1821,6 @@ int bind_builtin(WORD_LIST *list)
         return (EX_USAGE);
     }
 
-          // struct sockaddr_in {
-          //     sa_family_t    sin_family; /* address family: AF_INET */
-          //     in_port_t      sin_port;   /* port in network byte order */
-          //     struct in_addr sin_addr;   /* internet address */
-          // };
-
-          // /* Internet address. */
-          // struct in_addr {
-          //     uint32_t       s_addr;     /* address in network byte order */
-          // };
-
     if (bind(socketfd, &addr.addr, addrlen) == -1) {
         warn("bind: failed");
         return (EXECUTION_FAILURE);
